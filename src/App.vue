57f849dee79f3012b74de70ede455c1593ee8e76
daisuke shiraishi <kaiyujin@gmail.com>
2021-05-15 22:53:04 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p class="decode-result">Last result: <b>{{ result }}</b></p>
+
+    <qrcode-stream :camera="camera" @decode="onDecode" @init="onInit">
+      <div v-if="validationFailure" class="validation-failure">
+        This is NOT a URL!
+      </div>
+
+      <div v-if="validationPending" class="validation-pending">
+        Validation in progress...
+      </div>
+    </qrcode-stream>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { QrcodeStream } from 'vue-qrcode-reader'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  components: { QrcodeStream },
+
+  data () {
+    return {
+      isValid: undefined,
+      camera: 'auto',
+      result: null,
+    }
+  },
+
+  computed: {
+    validationPending () {
+      return this.isValid === undefined
+          && this.camera === 'off'
+    },
+
+    validationFailure () {
+      return this.isValid === false
+    }
+  },
+
+  methods: {
+
+    onInit (promise) {
+      promise
+          .catch(console.error)
+          .then(this.resetValidationState)
+    },
+
+    resetValidationState () {
+      this.isValid = undefined
+    },
+
+    async onDecode (content) {
+      this.result = content
+      this.turnCameraOff()
+
+      // pretend it's taking really long
+      await this.timeout(2000)
+      this.isValid = content.startsWith('http')
+      if ( this.isValid ) {
+        window.location = content
+      }
+
+      // some more delay, so users have time to read the message
+      await this.timeout(2000)
+
+      this.turnCameraOn()
+    },
+
+    turnCameraOn () {
+      this.camera = 'auto'
+    },
+
+    turnCameraOff () {
+      this.camera = 'off'
+    },
+
+    timeout (ms) {
+      return new Promise(resolve => {
+        window.setTimeout(resolve, ms)
+      })
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.validation-failure,
+.validation-pending {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(255, 255, 255, .8);
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-weight: bold;
+  font-size: 1.4rem;
+  padding: 10px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+}
+.validation-failure {
+  color: red;
 }
 </style>
